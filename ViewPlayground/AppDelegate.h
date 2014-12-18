e53c288d3f6f6b937d4a416f846c24e1f96a6b66
Oliver Kocsis <oliver.kocsis@imind.eu>
2014-12-18 17:34:39 +0100

    Initial Commit

---
+++ ViewPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewPlayground
+//
+//  Created by Kocsis Olivér on 2014.12.18..
+//  Copyright (c) 2014 swiftosis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewPlayground/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViewPlayground
+//
+//  Created by Kocsis Olivér on 2014.12.18..
+//  Copyright (c) 2014 swiftosis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
